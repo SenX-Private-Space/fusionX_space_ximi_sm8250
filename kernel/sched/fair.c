70fc11f4b32e723b2dd4238b3711c7154669579b
Muhammad Guntur <mochtur@gmail.com>
2025-07-10 20:23:15 +0700

    sched/fair: Move __pick_last_entity outside debug guard
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1036,7 +1036,6 @@ static struct sched_entity *pick_eevdf(struct cfs_rq *cfs_rq)
 	return __pick_eevdf(cfs_rq, true);
 }
 
-#ifdef CONFIG_SCHED_DEBUG
 struct sched_entity *__pick_last_entity(struct cfs_rq *cfs_rq)
 {
 	struct rb_node *last = rb_last(&cfs_rq->tasks_timeline.rb_root);
@@ -1047,6 +1046,7 @@ struct sched_entity *__pick_last_entity(struct cfs_rq *cfs_rq)
 	return __node_2_se(last);
 }
 
+#ifdef CONFIG_SCHED_DEBUG
 /**************************************************************
  * Scheduling class statistics methods:
  */
